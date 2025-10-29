f9f8b361ffd9289be701793739174a3607716a70
sasankgit <sasank4pr@gmail.com>
2025-10-29 15:26:38 +0530

    fixed error password anf importlib lines

--- osdag_gui/data/database/database_config.py
+++ osdag_gui/data/database/database_config.py
@@ -4,7 +4,7 @@ from osdag_core.Common import (KEY_DISP_FINPLATE, KEY_DISP_ENDPLATE, KEY_DISP_CL
                                KEY_DISP_BUTTJOINTWELDED, KEY_DISP_BASE_PLATE, KEY_DISP_TENSION_BOLTED, KEY_DISP_TENSION_WELDED, KEY_DISP_COMPRESSION_Strut,
                                KEY_DISP_COMPRESSION_COLUMN, KEY_DISP_FLEXURE, KEY_DISP_FLEXURE2, KEY_DISP_FLEXURE4, KEY_DISP_PLATE_GIRDER_WELDED)
 from pathlib import Path
-from importlib.resources import files
+from importlib_resources import files
 
 SQLITE_FILE = files('osdag_gui.data.database')/ 'user_data.sqlite'
 
